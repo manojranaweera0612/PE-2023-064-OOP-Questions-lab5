eb05c91b570b6650e7ac261be71e4ece56371fe6
manojranaweera0612 <manojranaweera9@gmail.com>
2026-06-02 13:45:00 +0530

    quection1

---
+++ lab07/src/main/java/com/mycompany/lab07/Lab07.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab07;
+
+/**
+ *
+ * @author dinus
+ */
+public class Lab07 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
