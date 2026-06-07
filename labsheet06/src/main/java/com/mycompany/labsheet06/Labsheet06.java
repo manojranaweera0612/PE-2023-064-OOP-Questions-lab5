5e7448a88c2c0eb0e912d7c157738c264315b949
manojranaweera0612 <manojranaweera9@gmail.com>
2026-06-07 11:41:05 +0530

    question

---
+++ labsheet06/src/main/java/com/mycompany/labsheet06/Labsheet06.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.labsheet06;
+
+/**
+ *
+ * @author dinus
+ */
+public class Labsheet06 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
